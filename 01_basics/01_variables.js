28fd26ba83429bcc16db8e23c91c84c39749a685
SuvarnaZ1990 <30748623+SuvarnaZ1990@users.noreply.github.com>
2025-06-05 17:21:27 +0000

    Variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let acccountState
+
+//accountId = 2  //Assigmment cannot be done to conat it cannot be changed
+console.log(accountId)
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "11111"
+accountCity ="mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity, acccountState]);
+
+/* java script compiler has some constant
+- const cannot be changed: Assigmment cannot be done to conat it cannot be changed
+- prefer not to use var due to issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatTypes.js
@@ -0,0 +1,26 @@
+"use strict";   //treat all js code as newer version of java script
+
+//alert("Hello");  //cannot use alert because we are using node js not browser
+//alert(2+3);//cannot use alert because we are using node js not browser
+
+console.log(3+3); console.log("Hello")
+
+//console.log(3+3) console.log("Hello")   //cannot use this because it is not a valid statement because of the missing semicolon
+
+let name = "Suvarna"
+let age = 25;
+let isMarried = true
+let salary = null; //null is a special value that represents no value, this is standalone value
+let address; //undefined is a special value that represents no value, this is a variable that has not been assigned a value
+
+
+/*
+symbol : used during react, when we need to work with unique identifiers, it is used
+*/
+console.log(name, age, isMarried, salary, address);
+
+console.log(typeof(name), typeof(age), typeof(isMarried), typeof(salary), typeof(address));
+
+
+
+
